e9ea500a8c7858ef8df7af5aa5101df7b331341c
rs-kumar-pk <123354372+rs-kumar-pk@users.noreply.github.com>
2023-04-10 16:24:29 -0400

    Add files via upload
    new.ps1

---
+++ new.ps1
@@ -0,0 +1,16 @@
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+
+
+Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm:$false 
+
+# Install required modules
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module pswindowsupdate -force
+Import-Module PSWindowsUpdate -force
+# End installing required modules
+
+#Get list of windows updates to install
+Get-WindowsUpdate
+
+#install the updates
+Install-WindowsUpdate -AcceptAll -install -AutoReboot
